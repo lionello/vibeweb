cbba587d4267d663d46bf5e71cada805d3a82c17
Lionello Lunesu <lio+git@lunesu.com>
2018-10-10 17:29:37 +0800

    Grab port from Azure

--- source/app.d
+++ source/app.d
@@ -2,16 +2,20 @@ import vibe.vibe;
 
 void main()
 {
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
+    import std.process : environment;
+    import std.conv;
 
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-	runApplication();
+    auto settings = new HTTPServerSettings;
+    immutable port = environment.get("HTTP_PLATFORM_PORT"); //Use the port IIS tells us to.
+    settings.port = port ? to!ushort(port) : 8080;
+    settings.bindAddresses = ["::1", "127.0.0.1"];
+    listenHTTP(settings, &hello);
+
+    logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+    runApplication();
 }
 
 void hello(HTTPServerRequest req, HTTPServerResponse res)
 {
-	res.writeBody("Hello, World!");
+    res.writeBody("Hello, World!");
 }
