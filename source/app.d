1f1a0827da4970a1e15cf8127a06795f8b0b847d
Lionello Lunesu <lio+git@lunesu.com>
2018-10-10 22:20:02 +0800

    Listen on all interfaces

--- source/app.d
+++ source/app.d
@@ -8,7 +8,7 @@ void main()
     auto settings = new HTTPServerSettings;
     immutable port = environment.get("HTTP_PLATFORM_PORT"); //Use the port IIS tells us to.
     settings.port = port ? to!ushort(port) : 8080;
-    settings.bindAddresses = ["::1", "127.0.0.1"];
+    settings.bindAddresses = ["0.0.0.0"];
     listenHTTP(settings, &hello);
 
     logInfo("Please open http://127.0.0.1:8080/ in your browser.");
